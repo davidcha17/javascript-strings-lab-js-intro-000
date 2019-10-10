735e9c0f576c4b5e80a7650ce59c7c30466dd2c6
David Cha <david.cha44@qmail.cuny.edu>
2019-10-10 19:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ var greetSpecialGuest = greet.concat(specialGuest).concat("!")
 
 var topic = "space";
 var sentence = 'Let\'s talk about '
+
 var conversation = (sentence).concat(topic).concat('.');
